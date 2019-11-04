93792b65c29999d444e7ddb74572fe7296e496d5
Ricky dildy <rdildy92@gmail.com>
2019-11-04 18:13:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,13 +15,19 @@ shipping_manifest == the_mainfest
 end
 
 def retrieval
-  shipping_manifest = {
+  shipping_manifest(the_mainfest) = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+   do     the_mainfest.index |"oil paintings"|
+        #=> returns price of "oil paintings"
+        
+       end 
+       
+       
+       
+  
 
 end
 
