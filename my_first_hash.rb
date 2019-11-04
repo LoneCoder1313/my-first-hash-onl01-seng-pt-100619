26f30248f00610ba19cd59711975427fc85270bc
Ricky dildy <rdildy92@gmail.com>
2019-11-04 17:56:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash{name ="Ricky", age = "27"}
+def my_hash{name =Ricky , age = 27}
   #my_hash= "Ricky", "27"
 
   
@@ -6,10 +6,7 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  
 
 end
 
