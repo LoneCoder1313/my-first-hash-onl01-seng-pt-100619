75e9937f3be04490fd75c27db96e321302beae88
Ricky dildy <rdildy92@gmail.com>
2019-11-04 17:54:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+def my_hash{name ="Ricky" age = "27"}
+  #my_hash= "Ricky", "27"
 
   
 end
